ef521d20ea77a8d59efb67abc7a9f9e23664b467
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-30 13:46:37 +0000

    Initial commit

---
+++ src/main/java/com/ageinghippy/cnuser_microservice/CnuserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ageinghippy.cnuser_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnuserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnuserMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ageinghippy/cnuser_microservice/CnuserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ageinghippy.cnuser_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnuserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
